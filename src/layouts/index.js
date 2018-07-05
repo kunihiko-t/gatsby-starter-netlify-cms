93723a285214de3c143adf26d28c9a324562fb4b
Kunihiko Tanaka <inverse.of.void.matrix@gmail.com>
2018-07-05 21:56:57 +0900

    Added social button

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,20 +1,27 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
+import React from "react";
+import PropTypes from "prop-types";
+import Helmet from "react-helmet";
 
-import Navbar from '../components/Navbar'
-import './all.sass'
+import Navbar from "../components/Navbar";
+import "./all.sass";
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Home | Gatsby + Netlify CMS" />
+    <Helmet title="Home | Gatsby + Netlify CMS">
+      <script
+        type="text/javascript"
+        src="https://b.st-hatena.com/js/bookmark_button.js"
+        charset="utf-8"
+        async="async"
+      />
+    </Helmet>
     <Navbar />
     <div>{children()}</div>
   </div>
-)
+);
 
 TemplateWrapper.propTypes = {
-  children: PropTypes.func,
-}
+  children: PropTypes.func
+};
 
-export default TemplateWrapper
+export default TemplateWrapper;
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,6 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { kebabCase } from "lodash";
+import {
+  FacebookShareButton,
+  GooglePlusShareButton,
+  LinkedinShareButton,
+  TwitterShareButton,
+  FacebookShareCount,
+  GooglePlusShareCount,
+  LinkedinShareCount,
+  FacebookIcon,
+  TwitterIcon,
+  GooglePlusIcon,
+  LinkedinIcon
+} from "react-share";
 import Helmet from "react-helmet";
 import Link from "gatsby-link";
 import Content, { HTMLContent } from "../components/Content";
@@ -16,6 +29,9 @@ export const BlogPostTemplate = ({
   helmet
 }) => {
   const PostContent = contentComponent || Content;
+  const iconSize = 36;
+  const filter = count => (count > 0 ? count : "");
+  const url = window.location.href;
 
   return (
     <section className="section">
@@ -28,6 +44,58 @@ export const BlogPostTemplate = ({
             </h1>
             <p>{description}</p>
             <PostContent content={content} />
+            <div style={{ marginTop: "20px" }} className="columns">
+              <div className="column is-narrow is-offset-4">
+                <TwitterShareButton url={url} title={title}>
+                  <TwitterIcon round size={iconSize} />
+                </TwitterShareButton>
+              </div>
+              <div className="column is-narrow">
+                <FacebookShareButton
+                  url={url}
+                  quote={`${title} - ${description}`}
+                  aria-label="Facebook share"
+                >
+                  <FacebookIcon round size={iconSize} />
+                  <FacebookShareCount url={url}>
+                    {count => (
+                      <div className="share-count">{filter(count)}</div>
+                    )}
+                  </FacebookShareCount>
+                </FacebookShareButton>
+              </div>
+              <div className="column is-narrow">
+                <LinkedinShareButton
+                  url={url}
+                  title={title}
+                  description={description}
+                >
+                  <LinkedinIcon round size={iconSize} />
+                  <LinkedinShareCount url={url}>
+                    {count => (
+                      <div className="share-count">{filter(count)}</div>
+                    )}
+                  </LinkedinShareCount>
+                </LinkedinShareButton>
+              </div>
+              <div className="column is-narrow">
+                <a
+                  href="http://b.hatena.ne.jp/entry/"
+                  className="hatena-bookmark-button"
+                  data-hatena-bookmark-layout="vertical-normal"
+                  data-hatena-bookmark-lang="en"
+                  title="このエントリーをはてなブックマークに追加"
+                >
+                  <img
+                    src="https://b.st-hatena.com/images/entry-button/button-only@2x.png"
+                    alt="このエントリーをはてなブックマークに追加"
+                    width="20"
+                    height="20"
+                    style={{ border: "none" }}
+                  />
+                </a>
+              </div>
+            </div>
             {tags && tags.length ? (
               <div style={{ marginTop: `4rem` }}>
                 <h4>Tags</h4>
