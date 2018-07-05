ad77eeae73b53f37c26a1d9c8b53e13ae4e01204
Kunihiko Tanaka <inverse.of.void.matrix@gmail.com>
2018-07-06 00:15:51 +0900

    Modified title

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -7,7 +7,7 @@ import "./all.sass";
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Home | Gatsby + Netlify CMS">
+    <Helmet title="Home | Blog.Valletta.io">
       <script
         type="text/javascript"
         src="https://b.st-hatena.com/js/bookmark_button.js"
