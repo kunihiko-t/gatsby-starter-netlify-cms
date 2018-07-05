dae028b0f0688235be10c6043ea45589c29ff2e8
Kunihiko Tanaka <inverse.of.void.matrix@gmail.com>
2018-07-05 18:04:51 +0900

    Removed product page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,9 +18,9 @@ const Navbar = () => (
         <Link className="navbar-item" to="/about">
           About
         </Link>
-        <Link className="navbar-item" to="/products">
+        {/* <Link className="navbar-item" to="/products">
           Products
-        </Link>
+        </Link> */}
       </div>
       <div className="navbar-end">
         <a
